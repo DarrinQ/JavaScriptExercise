c568a3c113f3ad5cbf774a9fe5673f3e3ee828cb
DarrinQ <agtina@126.com>
2017-07-08 22:25:18 +0800

    string & array exercise

---
+++ js/test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var month = 7, day = 8, year = 2017;
+var str = `today is ${month}-${day}-${year}.`; 
+alert(str);
+
+var ar = [1, 2.1, 'abc', true, null, {name:'bob', age:'19'}];
+ar[6] = '黄飞鸿';
+alert(`array is ${ar.slice(3)}.`);
+
+var copy_ar = ar.slice();
+copy_ar.sort()
+alert(`copy_ar is ${copy_ar.join('-')}; copy_ar is equal ar? ${copy_ar == ar}; copy_ar is ar? ${copy_ar === ar}.`);
